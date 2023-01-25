f0fefb7da45e02cde0e3956914d0f84ebd91d9ae
Cristian <cristianguaglianone@gmail.com>
2023-01-25 21:52:05 +0100

    starter

---
+++ src/lib/env.js
@@ -0,0 +1,4 @@
+export const APIKEY = ""; // Chiave api
+export const LANG = "it"; // Lingua italiana del JSON di risposta
+export const UNIT = "metric"; // Imposta i gradi a Celsius
+export const CNT = 3; // Numeri di intervallo di tempo per previsoni meteo
---
+++ src/pages/Weather.js
@@ -0,0 +1,3 @@
+export const Weather = () => {
+  return <div className="page weather_page"></div>;
+};
